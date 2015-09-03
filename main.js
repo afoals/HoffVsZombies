2e739c08c79b0239ad66cf9b0909ea079a85cea6
Emma Nash <emma.nash@pebblecode.com>
2015-09-03 16:59:17 +0100

    Add music

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  var loader = this;
+
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
+
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
+
 $(function() { //canvas variables
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -26,6 +73,50 @@ var totalRocks = 10;
 var rocks = [];
 for (var i = 0; i < totalRocks; i++) {
     addRock();
+}
+
+var audioContext;
+var audioBufferLoader;
+var audioBufferList;
+var ctls = [];
+var currentSoundCtl = 0;
+var hitCount = 0;
+
+function startAudio(buffers) {
+  function createSource(buffer) {
+    var source = audioContext.createBufferSource();
+    var gainNode = audioContext.createGain ? audioContext.createGain() : audioContext.createGainNode();
+    source.buffer = buffer;
+    source.loop = true;
+    source.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+
+    return {
+      source: source,
+      gainNode: gainNode
+    };
+  };
+  ctls.push(createSource(buffers[0]));
+  ctls.push(createSource(buffers[1]));
+  ctls.push(createSource(buffers[2]));
+  ctls.push(createSource(buffers[3]));
+
+  ctls[1].gainNode.gain.value = 0;
+  ctls[2].gainNode.gain.value = 0;
+  ctls[3].gainNode.gain.value = 0;
+
+  // Start playback in a loop
+  if (!ctls[0].source.start) {
+    ctls[0].source.noteOn(0);
+    ctls[1].source.noteOn(0);
+    ctls[2].source.noteOn(0);
+    ctls[3].source.noteOn(0);
+  } else {
+    ctls[0].source.start(0);
+    ctls[1].source.start(0);
+    ctls[2].source.start(0);
+    ctls[3].source.start(0);
+  }
 }
 
 function addRock() {
@@ -84,6 +175,17 @@ function animate() {
         if (isColliding(rock, block)) {
             score -= 10;
             resetRock(rock);
+            hitCount++;
+            if (hitCount == 5) {
+              hitCount = 0;
+              if (currentSoundCtl < 3) {
+                currentSoundCtl++;
+                console.log('incrementing sound ctl', currentSoundCtl);
+                ctls[currentSoundCtl].gainNode.gain.value = 1;
+              } else {
+                console.log('max sound ctl');
+              }
+            }
         }
 
         // advance the rocks
@@ -138,6 +240,14 @@ function drawAll() {
 
 // button to start the game
 $("#start").click(function () {
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    audioContext = new AudioContext();
+    audioBufferLoader = new BufferLoader(
+      audioContext,
+      ['samples/axel-1-drum.mp3', 'samples/axel-2-bass.mp3', 'samples/axel-3-beat.mp3', 'samples/axel-4-tune.mp3'],
+      startAudio);
+    audioBufferLoader.load();
+
     score = startingScore
     block.x = 0;
     for (var i = 0; i < rocks.length; i++) {
